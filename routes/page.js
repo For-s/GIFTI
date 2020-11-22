de79f991a01ca71a5ef92d713ae65fcce81c601a
minsun <meeeeensun@gmail.com>
2020-11-22 17:50:01 +0900

    2020-11-22 최종

--- routes/page.js
+++ routes/page.js
@@ -4,9 +4,12 @@ var router = express.Router();
 router.get('/main', (req, res) => {
   res.render('main', { title: 'main' });
 });
-
+//get('여기가 보여지는 url')
 router.get('/q1',(req,res)=>{
+    //render('여기가 실제 html파일')
     res.render('q1',{title: 'q1'});
 });
-  
+router.get('/q2',(req,res)=>{
+    res.render('q2',{title: 'q2'});
+});
 module.exports = router;
